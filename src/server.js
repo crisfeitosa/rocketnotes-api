bebb6c224db06dc7fe80655445fdbf90665b83eb
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-12 18:19:18 -0300

    Método GET

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/message', (request, response) => {
+  response.send('Hello, world!')
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
