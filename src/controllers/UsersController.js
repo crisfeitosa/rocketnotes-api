433a95e2b5ba72a8fb68d81de474eec270063d5f
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-13 22:28:04 -0300

    Tratando exceções

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+import AppError from '../utils/AppError.js';
+
 export default class UsersController {
   create(request, response) {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
+
+    if (!name) {
+      throw new AppError('Nome é obrigatório')
+    }
 
     response.status(201).json({ name, email, password })
   }
