63e58a8ccfd1ca4ce124719d85d5a6f5cc0a6ab7
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-13 22:02:00 -0300

    Controllers

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,9 @@
+export class UsersController {
+  /**Add commentMore actions
+   * index - GET para listar vários registros.
+   * show - GET para exibir um registro específico.
+   * create - POST para criar um registro.
+   * update - PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
+}
