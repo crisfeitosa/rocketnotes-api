a5609410bc50fd0c697e92b313f3c7a9ba4cd437
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-15 15:16:20 -0300

    Migrations para Link e Tag

---
+++ src/database/knex/migrations/20250615180944_createTags.js
@@ -0,0 +1,9 @@
+export const up = knex => knex.schema.createTable("tags", table => {
+  table.increments("id");
+  table.text("name").notNullable();
+
+  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
+  table.integer("user_id").references("id").inTable("users");
+});
+
+export const down = knex => knex.schema.dropTable("tags");
