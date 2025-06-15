f2f6b4483135e25c35204e8d7dd86e0ac765812c
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-15 16:28:45 -0300

    Listando Tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import usersRoutes from './users.routes.js';
 import notesRoutes from './notes.routes.js';
+import tagsRoutes from './tags.routes.js';
 
 const routes = Router();
 routes.use('/users', usersRoutes);
 routes.use('/notes', notesRoutes);
+routes.use('/tags', tagsRoutes);
 
 export default routes;
