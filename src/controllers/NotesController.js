71a6754dba0520a98e8e81a34830c9f42f8e5f25
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-15 15:34:06 -0300

    Exibindo nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -32,4 +32,18 @@ export default class NotesController {
 
     response.json();
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("notes").where({ id }).first();
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+
+    return response.json({
+      ...note,
+      tags,
+      links
+    });
+  }
 }
