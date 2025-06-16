c1f96e34c0bee05c673ef45db59aeb8500d8e423
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-16 19:08:16 -0300

    Validando usuário cadastrado

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,16 @@
+import knex from '../database/knex/index.js';
+import AppError from '../utils/AppError.js';
+
 export default class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
-    return response.json({ email, password });
+    const user = await knex('users').where({ email }).first();
+
+    if (!user) {
+      throw new AppError('Email e/ou senha incorreta', 401);
+    }
+
+    return response.json(user);
   }
 }
