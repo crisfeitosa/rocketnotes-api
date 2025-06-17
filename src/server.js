456a7fa1c4f472d1f51a1f6e9d7c6c7bf1e8a34b
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-17 19:31:10 -0300

    Cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import migrationsRun from './database/sqlite/migrations/index.js';
 
 import AppError from './utils/AppError.js';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 import uploadConfig from './configs/upload.js';
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
