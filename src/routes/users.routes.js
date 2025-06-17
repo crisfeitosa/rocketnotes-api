8081ee1dd75e43e41c376e9b0f42cb6ffaad5faa
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-17 19:19:30 -0300

    Salvando imagem no banco

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,18 +3,17 @@ import { Router } from 'express';
 
 import uploadConfig from '../configs/upload.js';
 import UsersController from '../controllers/UsersController.js';
+import UserAvatarController from '../controllers/UserAvatarController.js';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated.js';
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (req, res) => {
-  console.log(req.file.filename);
-  return res.json({ file: req.file.filename });
-})
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update);
 
 export default usersRoutes;
